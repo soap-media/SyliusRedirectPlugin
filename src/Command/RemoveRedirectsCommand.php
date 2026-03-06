c6f4aab8d9303274405633ee43fb8e502091b312
Aaron McLellan <aaron@soapmedia.co.uk>
2026-03-06 14:34:14 +0000

    Use #[AsCommand] attribute for console command

--- src/Command/RemoveRedirectsCommand.php
+++ src/Command/RemoveRedirectsCommand.php
@@ -5,23 +5,21 @@ declare(strict_types=1);
 namespace Setono\SyliusRedirectPlugin\Command;
 
 use Setono\SyliusRedirectPlugin\Repository\RedirectRepositoryInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'setono:sylius-redirect:remove',
+    description: 'This command will remove redirects that have not been accessed later than x days ago where x is the `setono_sylius_redirect.remove_after` parameter',
+)]
 class RemoveRedirectsCommand extends Command
 {
-    protected static $defaultName = 'setono:sylius-redirect:remove';
-
-    protected static $defaultDescription = 'This command will remove redirects that have not been accessed later than x days ago where x is the `setono_sylius_redirect.remove_after` parameter';
-
     private RedirectRepositoryInterface $redirectRepository;
 
     private int $removeAfter;
 
-    /**
-     * @param int $removeAfter The number of days that has to go before removing redirects
-     */
     public function __construct(RedirectRepositoryInterface $redirectRepository, int $removeAfter)
     {
         parent::__construct();
@@ -34,6 +32,6 @@ class RemoveRedirectsCommand extends Command
     {
         $this->redirectRepository->removeNotAccessed($this->removeAfter);
 
-        return 0;
+        return Command::SUCCESS;
     }
 }
